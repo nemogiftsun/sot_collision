fb5a14fcc866628a9d39ff404ddb2a2200fb644d
ngiftsun <nirmalgibbs@gmail.com>
2017-09-12 08:22:55 +0200

    scripts to check the latest implementation

---
+++ script/check.py
@@ -0,0 +1,34 @@
+import dynamic_graph.sotcollision as sc
+import numpy as np
+from dynamic_graph.sot.core.matrix_util import matrixToTuple
+ca = sc.SotCollision("sc")
+ca.proximitySensorDistance.value = (0.07,)*8
+
+p = np.zeros((6,8))
+c = 0
+for i in range(8):
+    for j in range(6):
+        c += 1
+        p[j,i] = c
+pose = matrixToTuple(p)
+
+
+Mi = ((1, 0.0, 0, 0.1), 
+      (0.0, 1.0, 0.0, 0.01),
+      (0, 0.0, 0.3, 0.2),
+      (0.0, 0.0, 0.0, 1.0))
+
+Ji = ((1.0, 0.0, 0.0, 0.0, 0.089159, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+     (0.0, 1.0, 0.0, -0.089159, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+     (0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+     (0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+     (0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+     (0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0))
+
+ca.jointTransformation.value = Mi
+ca.jointJacobian.value = Ji
+ca.proximitySensorPose.value = pose
+ca.setNumSkinSensors(8)
+ca.collisionDistance.recompute(1)
+ca.collisionJacobian.recompute(1)
+      
